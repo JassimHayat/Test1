aad6c6e9c76d9df9c0da6fa1983974aebbee009f
Jassim Hayat <jassim.hayat@gmail.com>
2025-07-31 23:47:21 +0300

    test 1 first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// function sayHello(name){
+// console.log(`hello  ${name}`)
+// }
+
+// sayHello("jassim");
+
+
+function addOne (num){
+    return 'this is so bad';
+    return num +8;
+}
+
+console.log(addOne(652))
